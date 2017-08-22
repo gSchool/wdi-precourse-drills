c6aa4791e3ef358dcc75237f71f92052ac99efb7
llamacoder <30612433+llamacoder@users.noreply.github.com>
2017-08-22 14:13:43 -0600

    Add personal website

---
+++ personalSite/js/personalSite.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  /* handle the click on the more buttons */
+  $("#morePreviousButton").on("click", function() {
+    /* toggle the hidden content and switch the button title */
+    if ($(this).html() == 'More') {
+      $(this).html("Less");
+      $(".previousToggleContent").fadeIn();
+    } else {
+      $(this).html("More");
+      $(".previousToggleContent").fadeOut();
+    }
+  });
+  $("#morePresentButton").on("click", function() {
+    /* toggle the hidden content and switch the button title */
+    if ($(this).html() == 'More') {
+      $(this).html("Less");
+      $(".presentToggleContent").fadeIn();
+    } else {
+      $(this).html("More");
+      $(".presentToggleContent").fadeOut();
+    }
+  });
+
+});
