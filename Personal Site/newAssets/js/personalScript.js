88dfeac1cb8adaf1e4d651ae0ed7beae9017671b
Matt <matthew.meyer29@gmail.com>
2017-10-15 19:42:44 -0600

    This is my personal site
    This is my personal site

---
+++ Personal Site/newAssets/js/personalScript.js
@@ -0,0 +1,10 @@
+function info() {
+var userInput =  prompt ("Are you sure you're ready to do this?")
+
+if (userInput !== null && userInput !== "") {
+  alert ("Awesome! Lets do some coding!")
+}else {
+  alert ("Unacceptable. You have to be ready!")}
+
+}
+;
