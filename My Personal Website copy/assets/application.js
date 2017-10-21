1f45da04e0982d3a908cb26b2b73e9fac5539e88
Mona Temchin <monatemchin@gmail.com>
2017-10-21 14:16:11 -0400

    Add files via upload
    Personal Website Precourse Exercise for Galvanize

---
+++ My Personal Website copy/assets/application.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+ $('button').on('click', function(){
+ $('.tenor-gif-embed').fadeOut();
+ $('button').fadeOut();
+ $('#purple-crap').fadeIn();
+ $('#there').fadeIn();
+ });
+
+});
