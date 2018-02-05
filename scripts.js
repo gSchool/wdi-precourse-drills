7baaecb56362e9b372c4fd4fd98a611a912fcf61
hunter-atk <33410523+hunter-atk@users.noreply.github.com>
2018-02-05 01:39:51 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+/*
+$("h1").click(function(){
+  if(clicked){
+      $(this).css('background-color', 'red');
+      clicked  = false;
+  } else {
+      $(this).css('background-color', 'blue');
+      clicked  = true;
+  }
+});
+*/
