709da685c4b50216e6e4117f4f6f1a1261f30b1f
dhausk <33533545+dhausk@users.noreply.github.com>
2018-02-20 01:14:30 -0700

    My actual Personal website

---
+++ personal website/main.js
@@ -0,0 +1,7 @@
+//javascript for main and sub
+$(document).ready(function(){
+  $('#gifs').on('click','button', function() {
+       //console.log('It worked');
+       $('#gify').toggle('.allTheGIFS');
+  });
+});
