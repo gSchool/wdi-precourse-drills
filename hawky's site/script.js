88fc220835312e3da4599deb5fb0f1b930987909
David Miller <36794601+Hawky454@users.noreply.github.com>
2018-04-25 12:13:47 -0500

    Added personal site
    Had to submit this earlier than I wanted to due to life circumstances.

---
+++ hawky's site/script.js
@@ -0,0 +1,17 @@
+
+
+document.getElementById("list").slideUp(3000).slideDown(2000);
+
+jQuery(document).ready(function() {
+
+$(".fade").slideUp(3000).slideDown(2000);
+
+
+  $(".btn").fadeOut(2000).fadeIn(1000);
+
+$(".contact").on("click", function() {
+  $(".old-school").slideUp(1000).slideDown(1000);
+});
+
+
+});
