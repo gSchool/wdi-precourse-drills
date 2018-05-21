8f89e3433cdc4aab8756bdf4ab9d2f638c42a56b
Chelsea Tompkins <31600601+ChelseaTompkins@users.noreply.github.com>
2018-05-20 19:28:57 -0700

    Update mySite.js

--- mySite/mySite.js
+++ mySite/mySite.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
   $('.overlayContent img').hide(0).delay(1000).fadeIn(2500).stop();
 
-  $('.aboutMeImages img').hover(function() {
-        $(this).addClass('transition');
-    }, function() {
-          $(this).removeClass('transition');
+  $('.aboutMeImages img').on("click", function() {
+      $(this).toggleClass('largeTransition');
     });
-});
+  });
