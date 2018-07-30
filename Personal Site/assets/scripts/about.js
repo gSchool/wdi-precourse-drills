5c639fdec900b0a86fa037f22a3ea1007b983379
tyler_pritchard <32420787+fantasticThingsToCome@users.noreply.github.com>
2018-07-29 22:28:02 -0700

    Add files via upload
    Added a personal site

---
+++ Personal Site/assets/scripts/about.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$(".gary").hide();
+	$(".ninethousand").hide();
+	$(".btn").click(function() {
+      $(".jake").hide(); 
+	});
+	$(".btn").click(function() {
+      $(".gary").show();
+	}); 
+});
